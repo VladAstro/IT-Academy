612e91d96a872978f0f3cffc149eb320cb3bfab0
VladAstro <90131863+VladAstro@users.noreply.github.com>
2021-12-23 15:45:02 +0300

    DZ_15

---
+++ DZ_15/index.js
@@ -0,0 +1,36 @@
+const body = document.querySelector('body')
+const div = document.createElement('div')
+const p = document.createElement('p')
+
+const users =
+ [
+  {
+    name: 'Vlad',
+    age: 15
+  },
+  {
+    name: 'Galy',
+    age: 16
+  },
+  {
+    name: 'Anton',
+    age: 17
+  },
+  {
+    name: 'Alexander',
+    age: 18
+  }
+];
+
+users.forEach(({name, age}, index) => {
+  const p = document.createElement('p');
+
+  p.innerText = `Name: ${name}, age: ${age}.`;
+
+  p.onclick = () => {
+    users.splice(index, -1)
+    body.removeChild(p)
+    
+  }
+ body.appendChild(p)
+});
